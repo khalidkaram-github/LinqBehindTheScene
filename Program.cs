e2ef57de8dbb41b2030bfc2c26910cac058b22ab
khalid karam <khalid.tasks@gmail.com>
2024-11-24 01:11:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LinqBehindTheScene
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result = numbers.Where(c => c > 5);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    public static class Collection
+    {
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource element in source)
+                if (predicate(element))
+                    yield return element;
+        }
+    }
+}
